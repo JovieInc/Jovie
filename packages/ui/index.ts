3470520130ae08eb0bba36461b89ef418c76d314
Tim White <35063371+itstimwhite@users.noreply.github.com>
2025-09-15 11:56:44 -0700

    feat: add upgrade link to user menu

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,8 +1,25 @@
 export type { ButtonProps } from './atoms/button';
 export { Button, buttonVariants } from './atoms/button';
 export {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from './atoms/dropdown-menu';
+export {
   Tooltip,
-  TooltipTrigger,
   TooltipContent,
   TooltipProvider,
+  TooltipTrigger,
 } from './atoms/tooltip';
