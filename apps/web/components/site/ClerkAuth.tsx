f66b89d09d2573ec0c20718ff6c327afcf1e90fa
Tim WHite <timwhite@mac.lan>
2026-03-21 01:24:29 -0700

    fix(auth): restore clerk legacy hooks

--- apps/web/components/site/ClerkAuth.tsx
+++ apps/web/components/site/ClerkAuth.tsx
@@ -1,21 +1,23 @@
 'use client';
 
-import { SignedIn, SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs';
+import { SignInButton, SignUpButton } from '@clerk/nextjs';
 import { UserButton } from '@/components/organisms/user-button';
+import { useAuthSafe } from '@/hooks/useClerkSafe';
 
 /**
  * Client-side component to render Clerk authentication buttons.
  */
 export default function ClerkAuth() {
+  const { isSignedIn } = useAuthSafe();
+
+  if (isSignedIn) {
+    return <UserButton />;
+  }
+
   return (
     <>
-      <SignedOut>
-        <SignInButton mode='redirect'>Sign In</SignInButton>
-        <SignUpButton mode='redirect'>Sign Up</SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      <SignInButton mode='redirect'>Sign In</SignInButton>
+      <SignUpButton mode='redirect'>Sign Up</SignUpButton>
     </>
   );
 }
