1623a3696592aff5ae10c485ab17ea7fc8f25b50
Tim White <t@timwhite.co>
2026-04-08 17:47:05 -0700

    refactor(public-surface): extract release shell primitives

---
+++ apps/web/components/organisms/public-surface/index.ts
@@ -0,0 +1,8 @@
+export type { PublicSurfaceFooterProps } from './PublicSurfaceFooter';
+export { PublicSurfaceFooter } from './PublicSurfaceFooter';
+export type { PublicSurfaceHeaderProps } from './PublicSurfaceHeader';
+export { PublicSurfaceHeader } from './PublicSurfaceHeader';
+export type { PublicSurfaceShellProps } from './PublicSurfaceShell';
+export { PublicSurfaceShell } from './PublicSurfaceShell';
+export type { PublicSurfaceStageProps } from './PublicSurfaceStage';
+export { PublicSurfaceStage } from './PublicSurfaceStage';
