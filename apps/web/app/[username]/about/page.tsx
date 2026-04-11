843b5d9a999cc61983bcc89956cbd8b7b8b810cf
Tim White <t@timwhite.co>
2026-04-10 20:00:28 -0700

    fix(web): avoid profile mode static params

--- apps/web/app/[username]/about/page.tsx
+++ apps/web/app/[username]/about/page.tsx
@@ -1,14 +1,9 @@
 import { redirectToProfileMode } from '../_lib/mode-route-redirect';
-import { getProfileStaticParams } from '../_lib/profile-static-params';
 
 interface Props {
   readonly params: Promise<{
     readonly username: string;
   }>;
-}
-
-export async function generateStaticParams() {
-  return getProfileStaticParams(100);
 }
 
 export default async function AboutPage({ params }: Props) {
