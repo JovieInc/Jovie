c3ac32ad64201262d685b8c31fd2ecdf9c0b6aa6
Tim White <t@timwhite.co>
2026-06-04 11:43:01 -0700

    fix(ui): tokenize avatar system b classes

--- packages/ui/lib/utils.ts
+++ packages/ui/lib/utils.ts
@@ -1,6 +1,27 @@
 import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge, validators } from 'tailwind-merge';
+
+const mergeTailwindClasses = extendTailwindMerge({
+  override: {
+    classGroups: {
+      'font-size': [
+        {
+          text: [
+            '3xs',
+            '2xs',
+            'app',
+            'mid',
+            'base',
+            validators.isTshirtSize,
+            validators.isArbitraryVariableLength,
+            validators.isArbitraryLength,
+          ],
+        },
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return mergeTailwindClasses(clsx(inputs));
 }
