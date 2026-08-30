4f14d675bbf63b0da7a2411a7f230da60da09ae5
Tim White <35063371+itstimwhite@users.noreply.github.com>
2026-08-30 02:14:01 +0000

    feat(cli): add public resource client (#16676)

---
+++ packages/jovie-cli/src/index.ts
@@ -0,0 +1,11 @@
+export {
+  DEFAULT_BASE_URL,
+  DEFAULT_TIMEOUT_MS,
+  fetchArtist,
+  fetchArtistLlms,
+  fetchOpenApi,
+  fetchSiteLlms,
+  JovieRequestError,
+  normalizeBaseUrl,
+  validateUsername,
+} from './client.js';
