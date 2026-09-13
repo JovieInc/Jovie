64027bf02d44e99c1742dca522c810da30377bb1
Tim White <35063371+itstimwhite@users.noreply.github.com>
2026-09-13 03:30:17 -0700

    fix(ui): remove new arbitrary switch variants

--- packages/ui/atoms/switch.test.tsx
+++ packages/ui/atoms/switch.test.tsx
@@ -323,9 +323,7 @@ describe('Switch', () => {
       );
       const switchElement = screen.getByTestId('switch');
       expect(switchElement).toHaveAttribute('aria-invalid', 'true');
-      expect(switchElement.className).toContain(
-        'aria-[invalid=true]:!border-error'
-      );
+      expect(switchElement.className).toContain('aria-invalid:!border-error');
     });
   });
 
